4977669d980108dc2571093a387e3a640019ab45
Sandeep Kumar <152271640+sandeepgraphy@users.noreply.github.com>
2025-07-08 15:12:59 +0530

    Add capacitor.config.ts 1751967779332

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bachu.app',
+  appName: 'Bachu',
+  webDir: 'out'
+};
+
+export default config;
